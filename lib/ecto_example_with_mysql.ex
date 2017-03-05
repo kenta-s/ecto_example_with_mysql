6d23dac0a1b51b89d1dcaad1f10b2b5e3cca21c5
kenta-s <knt01222@gmail.com>
2017-03-05 17:27:12 +0900

    Initial commit

---
+++ lib/ecto_example_with_mysql.ex
@@ -0,0 +1,18 @@
+defmodule EctoExampleWithMysql do
+  @moduledoc """
+  Documentation for EctoExampleWithMysql.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoExampleWithMysql.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_example_with_mysql_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoExampleWithMysqlTest do
+  use ExUnit.Case
+  doctest EctoExampleWithMysql
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
