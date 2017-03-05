d30ba52dbf4cee749cfd750e97c6dd34ad3f1e7e
kenta-s <knt01222@gmail.com>
2017-03-05 17:31:58 +0900

    Add some deps

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule EctoExampleWithMysql.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger],
+    [extra_applications: [:logger, :ecto, :mariaex],
      mod: {EctoExampleWithMysql.Application, []}]
   end
 
@@ -29,6 +29,7 @@ defmodule EctoExampleWithMysql.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:ecto, "~> 2.1"},
+     {:mariaex, ">= 0.0.0"}]
   end
 end
