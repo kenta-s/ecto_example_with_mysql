018a788527d6b2aa921dfb37d1b7dc3c313fab50
kenta-s <knt01222@gmail.com>
2017-03-05 19:12:49 +0900

    Add example of changeset

--- lib/ecto_example_with_mysql/person.ex
+++ lib/ecto_example_with_mysql/person.ex
@@ -7,6 +7,12 @@ defmodule EctoExampleWithMysql.Person do
     field :age, :integer
   end
 
+  # changeset will be used as validation.
+  # example:
+  #
+  # person = %EctoExampleWithMysql.Person{}
+  # changeset = EctoExampleWithMysql.Person.changeset(person, %{age: 29})
+  # EctoExampleWithMysql.Repo.insert(changeset)
   def changeset(person, params \\ %{}) do
     person
     |> Ecto.Changeset.cast(params, [:first_name, :last_name, :age])
